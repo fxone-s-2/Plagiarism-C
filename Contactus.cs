cf8ac7a79e42b1437042db335b0c3eb3ba78f827
Ann <annshi76@gmail.com>
2021-05-23 15:36:07 -0700

    Add project files.

---
+++ Contactus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Plagiarism_C
+{
+    public class Contactus
+    {
+        [Required(ErrorMessage ="Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+        
+    }
+}
