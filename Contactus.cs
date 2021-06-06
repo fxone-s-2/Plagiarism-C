f2bf5e8a14714d43d5c677561950244a7123ee91
Thunderdash454 <39601329+Thunderdash454@users.noreply.github.com>
2021-06-06 16:42:06 -0700

    Updating files

--- Contactus.cs
+++ Contactus.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FluentValidation;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -8,11 +9,18 @@ namespace Plagiarism_C
 {
     public class Contactus
     {
-        [Required(ErrorMessage ="Name is required")]
-        public string Name { get; set; }
 
-        [Required(ErrorMessage = "Email is required")]
+        public string Name { get; set; }
         public string Email { get; set; }
-        
+    }
+    public class ContactusValidator : AbstractValidator<Contactus>
+    {
+        public ContactusValidator()
+        {
+            RuleFor(p => p.Name).NotEmpty().WithMessage("Enter Your name:");
+            RuleFor(p => p.Name).MaximumLength(50).WithMessage("Name cannout longer than 50 char.");
+            RuleFor(p => p.Email).NotEmpty().WithMessage("Enter your email address:");
+            RuleFor(p => p.Email).EmailAddress().WithMessage("Please enter your a valid email address:");
+        }
     }
 }
